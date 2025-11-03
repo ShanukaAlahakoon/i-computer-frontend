e3fa23e329b7623eadc21253e2c2ec85bf3504b3
ShanukaAlahakoon <shanukaalahakoon456@gmail.com>
2025-11-03 14:17:31 +0530

    fix: Remove sensitive information from Test component

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -3,10 +3,7 @@ import { useState } from "react";
 export default function Test() {
   return (
     <div>
-      <h1>Test Component https://mvzithyzywzzbigfymch.supabase.co</h1>
-      eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12eml0aHl6eXd6emJpZ2Z5bWNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxNTIyMjksImV4cCI6MjA3NzcyODIyOX0.qCpBhrIrOSvmGol6xjIRaxaqs1WbcQYD40zqqbSXoDM
+      <h1>Test Component</h1>
     </div>
   );
 }
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12eml0aHl6eXd6emJpZ2Z5bWNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxNTIyMjksImV4cCI6MjA3NzcyODIyOX0.qCpBhrIrOSvmGol6xjIRaxaqs1WbcQYD40zqqbSXoDM
-//https://mvzithyzywzzbigfymch.supabase.co
